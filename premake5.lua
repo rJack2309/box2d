b8241f4ef95874d34528c29abaf9d0826be63167
bart <bpostepski@gmail.com>
2026-02-18 17:35:40 +0100

    Downgraded to v2.4.1 and added C++ Premake script

---
+++ premake5.lua
@@ -0,0 +1,37 @@
+project "Box2D"
+    kind "StaticLib"
+    language "C++"
+    cppdialect "C++17"
+    staticruntime "off"
+
+    targetdir ("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
+    objdir    ("%{wks.location}/bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files
+    {
+        "include/box2d/**.h",
+        "src/**.h",
+        "src/**.cpp"
+    }
+
+    includedirs
+    {
+        "include",
+        "src"
+    }
+
+    filter "system:windows"
+        systemversion "latest"
+        defines { "_CRT_SECURE_NO_WARNINGS" }
+
+    filter "configurations:Debug"
+        runtime "Debug"
+        symbols "on"
+
+    filter "configurations:Release"
+        runtime "Release"
+        optimize "on"
+
+    filter "configurations:Dist"
+        runtime "Release"
+        optimize "on"
